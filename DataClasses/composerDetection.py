fbe2a05182d95537dd85d9af54fba7a97adef7da
pipelonso <andresfelipeibanezcuta2@gmail.com>
2025-05-25 22:17:56 -0500

    Avances de UI y carga de imagen

---
+++ DataClasses/composerDetection.py
@@ -0,0 +1,21 @@
+class ComposerDetection:
+
+    def __init__(self,
+                 x1: int,
+                 y1: int,
+                 x2: int,
+                 y2: int,
+                 tag: str,
+                 command_name: str
+                 ):
+
+        self.x1: int = x1
+        self.y1: int = y1
+
+        self.x2: int = x2
+        self.y2: int = y2
+
+        self.tag: str = tag
+        self.command_name: str = command_name
+
+        pass
--- Views/Modules/UiComposer.py
+++ Views/Modules/UiComposer.py
@@ -1,6 +1,9 @@
 import customtkinter
 from typing import Any
 from globals import instances as global_instances
+from tkinter import filedialog
+from PIL import Image, ImageTk
+from DataClasses.composerDetection import ComposerDetection
 
 
 class UIComposer:
@@ -12,15 +15,26 @@ class UIComposer:
 
         self.master = master
         self.general_frame = customtkinter.CTkFrame(self.master)
+
         self.sidebar_frame = customtkinter.CTkFrame(global_instances.sidebar)
 
         self.mouse_pos_label = customtkinter.CTkLabel(self.sidebar_frame, text='', width=200, anchor='w')
         self.mouse_pos_label.pack(padx=2, pady=2, fill='x')
 
+        self.render_ui_image = None
+        self.detections_render_lis: list[ComposerDetection] = []
+
+        det = ComposerDetection(2, 2, 100, 100, 'test', 'test')
+        self.detections_render_lis.append(det)
+
         (customtkinter.CTkLabel(self.sidebar_frame, text='Archivo', anchor='w')
          .pack(padx=2, pady=2, fill='x'))
 
-        customtkinter.CTkButton(self.sidebar_frame, text='📁 Load file').pack(padx=2, pady=2, fill='x')
+        customtkinter.CTkButton(
+            self.sidebar_frame,
+            text='📁 Load file',
+            command=lambda:self.request_open()
+            ).pack(padx=2, pady=2, fill='x')
 
         (customtkinter.CTkLabel(self.sidebar_frame, text='Components', anchor='w')
          .pack(padx=2, pady=2, fill='x'))
@@ -44,6 +58,16 @@ class UIComposer:
         self.canvas = customtkinter.CTkCanvas(self.general_frame, cursor='cross')
         self.canvas.pack(padx=2, pady=2, fill='both', expand=True)
 
+        self.h_scrollbar = customtkinter.CTkScrollbar(self.canvas, orientation='horizontal',
+                                                      command=self.canvas.xview)
+        self.h_scrollbar.pack(side='bottom', fill='x')
+
+        self.v_scrollbar = customtkinter.CTkScrollbar(self.canvas, orientation='vertical',
+                                                      command=self.canvas.yview)
+        self.v_scrollbar.pack(side='right', fill='y')
+
+        self.canvas.configure(xscrollcommand=self.h_scrollbar.set, yscrollcommand=self.v_scrollbar.set)
+
         self.canvas.bind('<Motion>', self.mouse_motion_callback)
 
         pass
@@ -65,4 +89,33 @@ class UIComposer:
     def hide(self):
         self.general_frame.pack_forget()
         self.sidebar_frame.pack_forget()
+        pass
+
+    def update_render(self) :
+
+        if self.render_ui_image is not None:
+            self.canvas.create_image(0,0, image=self.render_ui_image, anchor='nw')
+            self.canvas.configure(scrollregion=self.canvas.bbox("all"))
+
+        for i in self.detections_render_lis:
+
+            self.canvas.create_rectangle(i.x1, i.y1, i.x2, i.y2, outline='red')
+
+            pass
+
+    def request_open(self):
+
+        path = filedialog.askopenfilename()
+
+        if path.endswith('.ckf3u'):
+
+            pass
+        else:
+            if path.endswith('.png'):
+                image = Image.open(path)
+                self.render_ui_image = ImageTk.PhotoImage(image)
+                self.update_render()
+
+
+
         pass
